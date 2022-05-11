875ba8a632fcf7d67f2525af88cb598899565238
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-11 19:52:52 +0300

    save

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const makeGallery = createItemsPicture(galleryItems);
+
+function createItemsPicture(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", makeGallery);
+
+gallery.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+    evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+   
+    const instance = basicLightbox.create(`
+    <img src="" class = "modal">
+`);
+    
+    instance.element().querySelector('.modal').src = evt.target.dataset.source;
+    instance.show();
+}
+
+
+
+
